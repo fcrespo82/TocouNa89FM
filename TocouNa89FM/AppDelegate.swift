8933a2b81a4a56d56e986b61500a04f046155aca
Fernando Crespo <fernando82@gmail.com>
2019-03-03 17:01:55 -0300

    Initial Commit

---
+++ TocouNa89FM/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TocouNa89FM
+//
+//  Created by Fernando Crespo on 03/03/19.
+//  Copyright © 2019 Fernando Crespo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TocouNa89FM/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TocouNa89FM
+//
+//  Created by Fernando Crespo on 03/03/19.
+//  Copyright © 2019 Fernando Crespo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
